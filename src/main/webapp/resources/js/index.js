288e007bc31d37e6a853fae1e3f4a22fd4534652
raon <khyoon@educo.co.kr>
2023-05-02 15:13:13 +0900

    no message

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -1,4 +1,5 @@
 
+
 const APP_ID = '3ee30362-8459-4ff8-b69c-091be04cfd7a';
 //const APP_ID='a277db9b-1280-4eda-a84a-333941a8b8c8';
 const DEMO_CHANNEL_ID = 'test3';
@@ -31,360 +32,6 @@ const avatarUser = [
 let client;
 let loginUserInfo = {};
 
-const addMessagehtml = function (message) {
-	return `
-    <div class="message-list wirter">
-		<div class="message-box">
-			<div class="message-time">
-				<div class="timestamps">${new Date(message.createdAt).toLocaleTimeString()}</div>
-				<div class="message-text">${message.text}</div>
-			</div>
-		</div>
-	</div>`;
-};
-
-const addFilePicturehtml = function (message) {
-	return `
-    <div class="message-list wirter">
-		<div class="message-box">
-			<div class="message-time">
-				<div class="timestamps">${new Date(message.createdAt).toLocaleTimeString()}</div>
-				<div class="file-area picture">
-					<a href="${message.fileUrl}" target="_blank"><img src="${message.fileUrl}" /></a>
-				</div>
-			</div>
-		</div>
-	</div>`;
-};
-
-
-
-$(document).ready(function () {
-	$('.chat-title').text(DEMO_CHANNEL_ID);
-	const today = new Date();
-	const year = today.getFullYear();
-	const month = today.getMonth() + 1;
-	const date = today.getDate();
-	const dateText = `${year}년 ${month >= 10 ? month : '0' + month}월 ${date >= 10 ? date : '0' + date}일`;
-	$('.chat-area .date').text(dateText);
-	
-	client = new TalkPlus.Client({appId: APP_ID});
-	
-	setupUsernameInputEventListener();
-	sendMessageInputListener(); //메시지 엔터 이벤트
-	sendMessageBtnListener(); //메세지이후 버튼 클릭이벤트.
-
-	
-	$("#attach_pic").on('change',function(event){ //파일첨부 이벤트
-		 sendFileImage(event);
-	});
-
-});
-
-function sendFileImage(event){
-	const file = event.target.files[0];
-	
-	//추가로 체크할 사항 : 파일확장자, 파일 사이즈등.
-	
-	convertBase64(file)
-	.then((imgBase64)=>{
-		console.log(imgBase64);
-		let imgFile = dataURLtoFile(imgBase64,'TalkPlusSampleImage');
-		let imgFileSize = String(imgFile.size);
-		//console.log(imgFile);
-		//console.log(imgFileSize);
-		client.sendMessage({
-			channelId: DEMO_CHANNEL_ID,
-			type: 'text',
-			text: '',
-			data: { 'fileSizeLabel': imgFileSize, 'fileNameLabel': imgFile.name, fileTypeLabel: 'image' },
-			file: imgFile,
-		}, function (err, data) {
-			if (err) {
-				return alert(err);
-			}
-			let html = '';
-			html = addFilePicturehtml(data.message);
-			$('.message-area').append(html);
-			scrollDown();
-		});
-		
-	})
-	.catch((err)=>{
-		alert(err);
-	})
-}
-
-function convertBase64(file){
-	return new Promise((resolve,reject)=>{
-		const fileReader = new FileReader();
-		fileReader.readAsDataURL(file);
-	    fileReader.onload = () => { resolve(fileReader.result); };
-	    fileReader.onerror = (error) => { reject(error); };
-	});
-}
-
-
-// 채팅방 기본설정
-function setupUsernameInputEventListener() {
-	// login anonymously
-	let pick = Math.floor(Math.random() * avatarUser.length);
-	client.loginAnonymous({userId: generateRandomId(), username: avatarUser[pick].username, profileImageUrl: avatarUser[pick].image}, function (errResp, data) {
-		loginUserInfo = data.user;
-
-		$('.user-box .user-img').attr('src', loginUserInfo.profileImageUrl);
-		$('.user-box .name input').val(loginUserInfo.username);
-
-		//user name 변경.
-		changeUserName(loginUserInfo);
-
-
-		if (errResp) {
-			return alert(JSON.stringify(errResp));
-		}
-		// join demo channel
-		client.joinChannel({channelId: DEMO_CHANNEL_ID}, function (errResp, data) {
-			if (errResp) {
-				if (errResp.code === '2003') { // if channel not found, create it
-					client.createChannel({
-						channelId: DEMO_CHANNEL_ID,
-						name: DEMO_CHANNEL_ID,
-						type: 'super_public',
-						members: []
-					}, function (errResp, data) {
-						if (errResp) {
-							return alert(JSON.stringify(errResp));
-						}
-					});
-				} else if (errResp.code === '2008') { // if user already had joined channel before, don't worry about error
-					// don't handle
-				} else {
-					return alert(JSON.stringify(errResp));
-				}
-			}
-
-			client.getMessages({channelId: DEMO_CHANNEL_ID}, function (errResp, data) {
-				if (errResp) {
-					return alert(JSON.stringify(errResp));
-				}
-				populateChatWindowWithMessages(data.messages);
-			});
-		});
-	});
- }
-
-//챗 설정
-/* promise
-function setupUsernameInputEventListener() {
-	// login anonymously
-	let pick = Math.floor(Math.random() * avatarUser.length);
-	
-	
-	client.loginAnonymous({
-		userId: generateRandomId(), 
-		username: avatarUser[pick].username, 
-		profileImageUrl: avatarUser[pick].image
-	})
-	.then((data)=> {
-	
-			console.log('data');
-			console.log(data);
-			console.log('data.user');
-			console.log(data.user);
-			loginUserInfo = data.user;
-		
-		loginUserInfo = data.user;
-
-		$('.user-box .user-img').attr('src', loginUserInfo.profileImageUrl);
-		$('.user-box .name input').val(loginUserInfo.username);
-		
-		client.joinChannel({channelId: DEMO_CHANNEL_ID})
-		.then(
-			(data)=>{
-			console.log('채널 들어가기');
-			client.getMessages({channelId: DEMO_CHANNEL_ID})
-			.then(data =>{
-				populateChatWindowWithMessages(data.messages);
-			})
-			.catch(err=>{
-				console.log('1'); 
-				console.log(JSON.stringify(err))
-			})
-		})
-		
-		.catch(err=> {
-			if(err.code==='2003'){
-				client.createChannel({  // if channel not found, create it
-					channelId: DEMO_CHANNEL_ID,
-					name: DEMO_CHANNEL_ID,
-					type: 'super_public',
-					members: []
-				})
-				.catch(err=> console.log(JSON.stringify(err))
-				)
-				
-			}else if(err.code==='2008'){}// if user already had joined channel before, don't worry about error
-			else{
-				console.log(JSON.stringify(err));
-			}
-		})
-	})
-	.catch(err=> console.log(JSON.stringify(err))
-	);
-	
- }*/
-
-
-function generateRandomString() {
-	return Math.floor((1 + Math.random()) * 0x10000)
-			.toString(16)
-			.substring(1);
-}
-
-function generateRandomId() {
-	return generateRandomString() + '-' + generateRandomString() + '-' + generateRandomString();
-}
-
-
-
-//파일 박스 뷰잉
-function showAttechFilebox(obj) {
-	
-	$(obj).addClass('active');
-	$(obj).closest('.message-write-inner').find('.attach-box').addClass('active');
-	
-	$(obj).on('focusout', function () {
-		$(this).removeClass('active');
-		$(this).closest('.message-write-inner').find('.attach-box').removeClass('active');
-	});
-}
-
-//파일첨부
-function selectFile(obj){
-	$(obj).siblings("input[name^=attach_file]").val(null);
-	$(obj).siblings("input[name^=attach_file]").click();
-}
-
-
-//파일 이미지 전송
-//function sendFileImage() {
-//	$('.attach-box .attach.pic').on('click', function () {
-//		let imgFile = dataURLtoFile(imgBase64,'TalkPlusSampleImage');
-//		let imgFileSize = String(imgFile.size);
-//		client.sendMessage({
-//			channelId: DEMO_CHANNEL_ID,
-//			type: 'text',
-//			text: '',
-//			data: { 'fileSizeLabel': imgFileSize, 'fileNameLabel': imgFile.name, fileTypeLabel: 'image' },
-//			file: imgFile,
-//		}, function (err, data) {
-//			if (err) {
-//				return alert(err);
-//			}
-//			let html = '';
-//			html = addFilePicturehtml(data.message);
-//			$('.message-area').append(html);
-//			scrollDown();
-//		});
-//	});
-//}
-
-
-// 닉네임 변경
-function changeUserName(loginUserInfo){
-	
-	$(".btn-alter").on("click",function(e){
-		$(this).closest('.user-box').find('.name').addClass('active');
-		$(this).closest('.user-box').find('.name input').focus();		
-	});
-	$(".user-box .name input").on("focusout",function(e){
-		let newUserName = $(this).closest('.user-box').find('.name input').val();
-		if (newUserName !== loginUserInfo.username){
-			client.updateUser({
-				username: newUserName,
-			});
-		}
-		$(this).closest('.user-box').find('.name').removeClass('active');		
-	});
-}
-
-// 엔터 입력시 메시지 이벤트
-function sendMessageInputListener() {
-	
-	
-	$(".enterMessage").on("keypress", function(e){
-		if ($('.enterMessage').val() !== ''){
-			if (e.keyCode === 13) {
-				e.preventDefault();
-				const messageText = $('.enterMessage').val();
-				$('.enterMessage').val('');
-				addMessageText(messageText);
-			}
-		}
-	});
-}
-
-//전송버튼 클릭시 이벤트
-function sendMessageBtnListener() {
-	
-	$("#btnEnterMessage").on("click",function(e){
-		e.preventDefault();
-		if ($('.enterMessage').val() !== '') {
-			const messageText = $('.enterMessage').val();
-			$('.enterMessage').val('');
-			addMessageText(messageText);
-		}
-	});
-}
-
-function populateChatWindowWithMessages(messages) {
-	let customMessages = messages.slice(-8, messages.length);
-	for (let i = customMessages.length - 1; i >= 0; i--) {
-		const message = messages[i];
-		let html = '';
-		html = avatarhtml(message);
-		$('.message-area').append(html);
-	}
-	scrollDown();
-}
-
-
-//메세지 전송
-function addMessageText(messageText) {
-	client.sendMessage({channelId: DEMO_CHANNEL_ID, type: 'text', text: messageText}, function (err, data) {
-		if (err) {
-			return alert(err);
-		}
-		let html = '';
-		html = addMessagehtml(data.message);
-		$('.message-area').append(html);
-		scrollDown();
-	});
-}
-
-function addMessage(message) {
-	let html = '';
-	html = avatarhtml(message);
-	$('.message-area').append(html);
-	scrollDown();
-}
-
-function scrollDown() {
-	$('#chatView').scrollTop($('#chatView')[0].scrollHeight);
-}
-
-function dataURLtoFile (dataurl, fileName) {
-	var arr = dataurl.split(','),
-			mime = arr[0].match(/:(.*?);/)[1],
-			bstr = atob(arr[1]),
-			n = bstr.length,
-			u8arr = new Uint8Array(n);
-
-	while(n--){
-			u8arr[n] = bstr.charCodeAt(n);
-	}
-	return new File([u8arr], fileName, {type:mime});
-}
 
 
 
@@ -459,4 +106,330 @@ const avatarhtml = function (message) {
 	}
 	return template;
 };
+
+
+
+const addMessagehtml = function (message) {
+	return `
+    <div class="message-list wirter">
+		<div class="message-box">
+			<div class="message-time">
+				<div class="timestamps">${new Date(message.createdAt).toLocaleTimeString()}</div>
+				<div class="message-text">${message.text}</div>
+			</div>
+		</div>
+	</div>`;
+};
+
+const addFilePicturehtml = function (message) {
+	return `
+    <div class="message-list wirter">
+		<div class="message-box">
+			<div class="message-time">
+				<div class="timestamps">${new Date(message.createdAt).toLocaleTimeString()}</div>
+				<div class="file-area picture">
+					<a href="${message.fileUrl}" target="_blank"><img src="${message.fileUrl}" /></a>
+				</div>
+			</div>
+		</div>
+	</div>`;
+};
+
+const addFileVideohtml = function (message) {
+	return `
+    <div class="message-list wirter">
+		<div class="message-box">
+			<div class="message-time">
+				<div class="file-area video">
+					<div class="timestamps">${new Date(message.createdAt).toLocaleTimeString()}</div>
+					<video width="304" controls>
+						<source src="${message.fileUrl}" type="video/mp4">
+					</video>
+				</div>
+			</div>
+		</div>
+	</div>`;
+};
+
+const addFilehtml = function (message) {
+	return `
+    <div class="message-list wirter">
+		<div class="message-box">
+			<div class="message-time">
+				<div class="timestamps">${new Date(message.createdAt).toLocaleTimeString()}</div>
+				<div class="file-area file">
+					<a href="${message.fileUrl}" target="_blank">
+						<sapn class="fileName">${message.data.fileNameLabel}</sapn>
+						<sapn class="fileSize">용량 : ${message.data.fileSizeLabel} Byte</sapn>
+						<sapn class="filedown"></sapn>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>`;
+};
+
+
+$(document).ready(function () {
+	//$('.chat-title').text(DEMO_CHANNEL_ID);
+	const today = new Date();
+	const year = today.getFullYear();
+	const month = today.getMonth() + 1;
+	const date = today.getDate();
+	const dateText = `${year}년 ${month >= 10 ? month : '0' + month}월 ${date >= 10 ? date : '0' + date}일`;
+	$('.chat-area .date').text(dateText);
+
+	client = new TalkPlus.Client({appId: APP_ID});
+
+	client.on('event', function (payload) {
+		if (payload.type === 'message') {
+			addMessage(payload.message);
+		}
+	});
+	setupUsernameInputEventListener();
+	sendMessageInputListener();
+	sendMessageBtnListener();
+
+	$("#attach_pic").on('change',function(event){ //파일첨부 이벤트
+	 sendFileImage(event);
+	});
+	
+});
+
+
+//파일첨부
+function selectFile(obj){
+	$(obj).siblings("input[name^=attach_file]").val(null);
+	$(obj).siblings("input[name^=attach_file]").click();
+}
+
+
+//파일 박스 뷰잉
+function showAttechFilebox(obj) {
+	
+	$(obj).addClass('active');
+	$(obj).closest('.message-write-inner').find('.attach-box').addClass('active');
+	
+	$(obj).on('focusout', function () {
+		$(this).removeClass('active');
+		$(this).closest('.message-write-inner').find('.attach-box').removeClass('active');
+	});
+}
+
+function sendFileImage(event){
+	const file = event.target.files[0];
+	
+	//추가로 체크할 사항 : 파일확장자, 파일 사이즈등(15mb) 
+	
+	
+	convertBase64(file)
+	.then((imgBase64)=>{
+		console.log(imgBase64);
+		let imgFile = dataURLtoFile(imgBase64,'TalkPlusSampleImage');
+		let imgFileSize = String(imgFile.size);
+		//console.log(imgFile);
+		//console.log(imgFileSize);
+		client.sendMessage({
+			channelId: DEMO_CHANNEL_ID,
+			type: 'text',
+			text: '',
+			data: { 'fileSizeLabel': imgFileSize, 'fileNameLabel': imgFile.name, fileTypeLabel: 'image' },
+			file: imgFile,
+		}, function (err, data) {
+			if (err) {
+				return alert(err);
+			}
+			let html = '';
+			html = addFilePicturehtml(data.message);
+			$('.message-area').append(html);
+			scrollDown();
+		});
+		
+	})
+	.catch((err)=>{
+		alert(err);
+	})
+}
+
+function convertBase64(file){
+	return new Promise((resolve,reject)=>{
+		const fileReader = new FileReader();
+		fileReader.readAsDataURL(file);
+	    fileReader.onload = () => { resolve(fileReader.result); };
+	    fileReader.onerror = (error) => { reject(error); };
+	});
+}
+
+
+
+function setupUsernameInputEventListener() {
+	// login anonymously
+	let pick = Math.floor(Math.random() * avatarUser.length);
+	client.loginAnonymous({userId: generateRandomId(), username: avatarUser[pick].username, profileImageUrl: avatarUser[pick].image}, function (errResp, data) {
+		loginUserInfo = data.user;
+
+		$('.user-box .user-img').attr('src', loginUserInfo.profileImageUrl);
+		$('.user-box .name input').val(loginUserInfo.username);
+
+		//user name 변경.
+		changeUserName(loginUserInfo);
+
+
+		if (errResp) {
+			return alert(JSON.stringify(errResp));
+		}
+		// join demo channel
+		client.joinChannel({channelId: DEMO_CHANNEL_ID}, function (errResp, data) {
+			if (errResp) {
+				if (errResp.code === '2003') { // if channel not found, create it
+					client.createChannel({
+						channelId: DEMO_CHANNEL_ID,
+						name: DEMO_CHANNEL_ID,
+						type: 'super_public',
+						members: []
+					}, function (errResp, data) {
+						if (errResp) {
+							return alert(JSON.stringify(errResp));
+						}
+					});
+				} else if (errResp.code === '2008') { // if user already had joined channel before, don't worry about error
+					// don't handle
+				} else {
+					return alert(JSON.stringify(errResp));
+				}
+			}
+
+			client.getMessages({channelId: DEMO_CHANNEL_ID}, function (errResp, data) {
+				if (errResp) {
+					return alert(JSON.stringify(errResp));
+				}
+				populateChatWindowWithMessages(data.messages);
+			});
+		});
+	});
+ }
+
+//닉네임 변경
+function changeUserName(loginUserInfo){
+	
+	$(".btn-alter").on("click",function(e){
+		$(this).closest('.user-box').find('.name').addClass('active');
+		$(this).closest('.user-box').find('.name input').focus();		
+	});
+	$(".user-box .name input").on("focusout",function(e){
+		let newUserName = $(this).closest('.user-box').find('.name input').val();
+		if (newUserName !== loginUserInfo.username){
+			client.updateUser({
+				username: newUserName,
+			});
+		}
+		$(this).closest('.user-box').find('.name').removeClass('active');		
+	});
+}
+
+function sendMessageInputListener() {
+	
+	$(".enterMessage").on("keypress", function(e){
+		if ($('.enterMessage').val() !== ''){
+			if (e.keyCode === 13) {
+				e.preventDefault();
+				const messageText = $('.enterMessage').val();
+				$('.enterMessage').val('');
+				addMessageText(messageText);
+			}
+		}
+	});
+}
+
+function sendMessageBtnListener() {
+	
+	$("#btnEnterMessage").on("click",function(e){
+		e.preventDefault();
+		if ($('.enterMessage').val() !== '') {
+			const messageText = $('.enterMessage').val();
+			$('.enterMessage').val('');
+			addMessageText(messageText);
+		}
+	});
+	
+}
+
+function populateChatWindowWithMessages(messages) {
+	let customMessages = messages.slice(-8, messages.length);
+	for (let i = customMessages.length - 1; i >= 0; i--) {
+		const message = messages[i];
+		let html = '';
+		html = avatarhtml(message);
+		$('.message-area').append(html);
+	}
+	scrollDown();
+}
+
+function addMessageText(messageText) {
+	client.sendMessage({channelId: DEMO_CHANNEL_ID, type: 'text', text: messageText}, function (err, data) {
+		if (err) {
+			return alert(err);
+		}
+		let html = '';
+		html = addMessagehtml(data.message);
+		$('.message-area').append(html);
+		scrollDown();
+	});
+}
+
+function addMessage(message) {
+	let html = '';
+	html = avatarhtml(message);
+	$('.message-area').append(html);
+	scrollDown();
+}
+
+function generateRandomString() {
+	return Math.floor((1 + Math.random()) * 0x10000)
+			.toString(16)
+			.substring(1);
+}
+
+function generateRandomId() {
+	return generateRandomString() + '-' + generateRandomString() + '-' + generateRandomString();
+}
+
+function scrollDown() {
+	$('#chatView').scrollTop($('#chatView')[0].scrollHeight);
+}
+
+
+function b64toBlob (b64Data, contentType = '', sliceSize = 512) {
+	const byteCharacters = atob(b64Data);
+	const byteArrays = [];
+
+	for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+		const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+		const byteNumbers = new Array(slice.length);
+		for (let i = 0; i < slice.length; i++) {
+			byteNumbers[i] = slice.charCodeAt(i);
+		}
+
+		const byteArray = new Uint8Array(byteNumbers);
+		byteArrays.push(byteArray);
+	}
+
+	const blob = new Blob(byteArrays, { type: contentType });
+	return blob;
+}
+
+function dataURLtoFile (dataurl, fileName) {
+	var arr = dataurl.split(','),
+			mime = arr[0].match(/:(.*?);/)[1],
+			bstr = atob(arr[1]),
+			n = bstr.length,
+			u8arr = new Uint8Array(n);
+
+	while(n--){
+			u8arr[n] = bstr.charCodeAt(n);
+	}
+	return new File([u8arr], fileName, {type:mime});
+}
+
 
